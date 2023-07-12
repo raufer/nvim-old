bc2be054b04bb83ebb0cd5453107ccfcd876c707
Raul Ferreira <raul.ferreira@nationalgrid.com>
2023-07-12 12:58:26 +0100

    prob next year

--- lua/user/ale.lua
+++ lua/user/ale.lua
@@ -11,3 +11,18 @@ vim.g.ale_lint_on_enter = 0
 vim.g.ale_lint_on_save = 1
 vim.g.ale_sign_error = '●'
 vim.g.ale_sign_warning = '.'
+
+vim.g.ale_fix_on_save = 1
+vim.g.ale_linters_explicit = 1
+
+vim.g.ale_linters = { python = { "flake8", "pylint", "mypy" } }
+
+vim.g.ale_fixers = {
+    ['*'] = {},
+    python = {'black', 'isort'},
+    sh = {'shfmt'},
+    zsh = {'shfmt'},
+}
+
+vim.g.ale_python_isort_options = "--profile black"
+
